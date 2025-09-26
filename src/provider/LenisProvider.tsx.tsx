9d8e498aa37b1a354e520000153d0b5398c735de
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-09-26 11:56:02 +0100

    Add smooth mouse scroll functionality

---
+++ src/provider/LenisProvider.tsx.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+
+interface LenisProviderProps {
+    children: ReactNode;
+}
+
+export default function LenisProvider({ children }: LenisProviderProps) {
+    useEffect(() => {
+        const lenis = new Lenis({
+            lerp: 0.1,
+        });
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
+
+    return <>{children}</>;
+}
