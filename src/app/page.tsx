0933c3e21167f9d001bd3fbeab60d6e705911bd4
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-10-06 10:50:33 +0100

    Create banner-2 section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Achievements from "@/components/home/achievements";
 import AnimatedCards from "@/components/home/animated-cards";
 import Banner from "@/components/home/banner";
+import Banner2 from "@/components/home/banner-2";
 import Benefits from "@/components/home/benefits";
 import Hero from "@/components/home/hero";
 import SlideSection from "@/components/home/picture-slide";
@@ -19,5 +20,6 @@ export default function Home() {
       <Benefits />
     </div>
     <Projects />
+    <Banner2 />
   </>
 }
---
+++ src/components/home/banner-2/index.tsx
@@ -0,0 +1,50 @@
+import BlurText from "@/components/shared/blur-text";
+
+export default function Banner2() {
+    return (
+        <section
+            className="min-h-[500px] lg:h-[1010px] xl:max-h-[1100px] px-8 xl:px-0 mt-32 flex justify-center relative bg-gradient-to-b from-[#05050555] to-[#000000]/50 lg:to-[#000000]">
+            <div className="w-full h-full lg:flex absolute -z-10 bg-cover bg-center bg-fixed hidden">
+                {Array.from({ length: 3 }).map((_, i) => {
+                    return (
+                        <div
+                            key={i}
+                            className="w-1/3 h-full overflow-hidden">
+                            <div
+                                key={i}
+                                style={{ backgroundImage: `url(/banner-2/pic-${i + 1}.avif)` }}
+                                className={`w-full h-full bg-cover bg-center ${(i === 0 || i === 2) ? "scale-125 bg-top" : ""}`}
+                            >
+
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <div className="w-full h-full flex justify-center lg:hidden absolute -z-10 bg-black">
+                <div className="w-[490px] sm:w-[450px] h-[450px] overflow-hidden py-24 px-[100px]">
+                    <div
+                        style={{ backgroundImage: "url(/banner-2/pic-3.avif)" }}
+                        className="w-full h-full bg-cover bg-center scale-125 rounded-md">
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-full sm:w-[500px] lg:max-w-[1220px] flex flex-col justify-center items-center gap-4 mt-[400px] lg:mt-0 lg:pt-36 text-center text-white">
+                <div className="flex items-start gap-3">
+                    <p className=" text-[#ff6200] text-3xl">
+                        &#47;&#47;
+                    </p>
+                    <BlurText
+                        text="Quanta isn't just a design agency; it's a powerhouse of innovation where strategy meets creativity, crafting visually stunning and highly effective digital experiences that captivate audiences, elevate brands, and drive real results."
+                        delay={70}
+                        animateBy="words"
+                        direction="top"
+                        className="text-[24px] lg:text-3xl mb-8 lg:w-[320px] xl:w-[300px] leading-7 lg:leading-9 text-gray-300"
+                    />
+                </div>
+            </div>
+        </section>
+
+    );
+}
